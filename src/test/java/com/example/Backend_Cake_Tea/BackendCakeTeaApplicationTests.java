93320f589f16657bd9666cd5066a57a34f3d2cd4
Phan Tiến Thnh <phantienthinh0907@gmail.com>
2026-07-06 11:16:01 +0700

    Update First code

---
+++ src/test/java/com/example/Backend_Cake_Tea/BackendCakeTeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend_Cake_Tea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCakeTeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
